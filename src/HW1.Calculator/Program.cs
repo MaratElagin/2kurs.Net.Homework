54f6ad62db8643c399ba37c12b99fdedfda55e4d
Marat Elagin <maratelagin@gmail.com>
2021-09-18 13:50:10 +0300

    Almost completed task: Calculator, InputParser, Tests

---
+++ src/HW1.Calculator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static HW1.Calculator.Calculator;
+
+namespace HW1.Calculator
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            var parseResult = InputParser.TryParseArguments(args, out var val1,
+                out var operation, out var val2);
+            if (parseResult == 0)
+            {
+                Console.WriteLine($"{val1} {GetDescription(operation)} {val2} = {Calculator.Calculate(val1, operation, val2)}");
+                return 0;
+            }
+
+            return parseResult;
+        }
+    }
+}
