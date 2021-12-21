2d8ec44983cd27f8229f1a29d15bcb06c5d8aa04
Marat Elagin <maratelagin@gmail.com>
2021-12-21 17:53:15 +0300

    Benchmark task completed

---
+++ HW12/HW12/CustomWebApplicationFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace HW12
+{
+    public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
+    {
+        protected override IHostBuilder CreateHostBuilder() =>
+            Host.CreateDefaultBuilder()
+                .ConfigureWebHostDefaults(a => a
+                    .UseStartup<TStartup>()
+                    .UseTestServer());
+    }
+}
---
+++ HW12/HW12/Program.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Running;
+using hw12;
+
+namespace HW12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<SpeedTests>();
+        }
+    }
+}
