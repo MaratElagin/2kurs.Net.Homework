799992c09f6fe97627739795ec88809568248b5c
Marat Elagin <maratelagin@gmail.com>
2022-01-01 14:34:28 +0300

    HW 13. Memory benchmarks and InMemory Calculator

---
+++ HW13/HW13/MethodsForBenchmark.cs
@@ -0,0 +1,12 @@
+﻿namespace HW13
+{
+    public class MethodsForBenchmark
+    {
+        public string Simple(string s) => s + s;
+        public virtual string Virtual(string s) => s + s;
+        public static string Static(string s) => s + s;
+        public string Generic<T>(T s) => s.ToString() + s;
+        public string Dynamic(dynamic s) => s.ToString() + s;
+        public string Reflection(string s) => s + s;
+    }
+}
